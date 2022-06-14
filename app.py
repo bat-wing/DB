51c8d48b6509c65a3739fb67a8474e25b421e00b
bat-wing <fanyichao0405@163.com>
2022-06-15 06:04:20 +0800

    index

--- app.py
+++ app.py
@@ -4,15 +4,30 @@
 # @Author: fyc
 # @Software: VScode
 
-from flask import Flask
+from flask import Flask, jsonify, request, render_template, make_response, send_from_directory, redirect, flash
+from flask_login import LoginManager, login_required, login_user, logout_user
+
 
 app = Flask(__name__)
 app.config['ENV'] = 'development'  # 添加这一句！！！将环境设置为开发环境
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def login():
+    return render_template('login.html')
+
+
+@app.route("/main", methods=['POST', 'GET'])
+def main():
+    return render_template('main.html')
+
+
+@app.route("/student", methods=['POST', 'GET'])
+def student():
+    lengeth = 1
+    l = [2019217844, 'fyc', '男', '21', '汉',
+         '计算机与信息系', '物联网工程', '19-2', '2019-9-1']
+    return render_template('index.html', len=lengeth, l=l)
 
 
 if __name__ == '__main__':
